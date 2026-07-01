f2bdf3473d3c96a76cec79fae1467bf83f488be0
xndrzx <frgttn.drxzm@gmail.com>
2026-06-30 23:58:48 -0400

    Co-authored-by: Emiliano García <ashraahx@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = createRoot(
+const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );
